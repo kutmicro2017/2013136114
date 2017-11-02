be6189208425abe78da2c061a6dcd956ef1c40b0
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-11-02 20:48:09 +0900

    Update TEST.ino

--- TEST.ino
+++ TEST.ino
@@ -16,11 +16,11 @@ const int row1[8] = { 38,39,36,37,34,35,32,33 };  //사람
 const int col2[8] = { 46,47,44,45,42,43,40,41};
 const int row2[8] = { 60,61,62,63,64,65,66,67};   //숫자
 
-int numberd = 0;//??
-int timec = 0;  //추가시간?
+int numberd = 0;// 신호등 시간
+int timec = 0;  // 리셋 시간 관리
 int bstate = 0; // <보행자신호> 0 = 아무것도 안눌림 1 = 1번 라인일때 2 = 2번 라인일때
 int sstate = 0; //  <상하 // 좌우> 신호 구분
-int times=0;
+
 
 int signImage[2][8][8] = {
   { //stop
@@ -187,13 +187,13 @@ void loop() {
   delay_a(TimeG);         
   digitalWrite(G1, LOW);
   if(bstate == 0){                 //보행자 신호를 입력 받지않았을 경우
-    for(int i = 0; i<TimeY; i++){
+     for(int i = 0; i<TimeY; i++){
      digitalWrite(Y1, HIGH);
-     delay_a(500);
+     delay_b(500);
      digitalWrite(Y1, LOW);
-     delay_a(500);
+     delay_b(500);
    }
-  }else if(bstate == 1){    //보행자 신호를 입력 받았을 경우 스위치가 눌렸을 경우 bstate=1;
+  }else if(bstate == 1){            //보행자 신호를 입력 받았을 경우 스위치가 눌렸을 경우 bstate=1;
     for(int i = 0; i<3; i++){
      digitalWrite(Y1, HIGH);            //이 부분이 필요가 없는거 같은데
      delay_a(500);
@@ -203,7 +203,7 @@ void loop() {
    bstate = 0;
   }
   digitalWrite(R1, HIGH);
-  //전환
+ ////////////////////////////////////////////// //전환
   sstate = 2; // 2번라인             
   numberd = 10;                     //이게 숫자 신호등의 시간인거 같아
   digitalWrite(R2, LOW);
@@ -213,6 +213,7 @@ void loop() {
     numberd = 10;                   //돌아가는 딜레이네
     delay_a(TimeG);                 //이게 리셋이구나
   }
+   cMat2();
   digitalWrite(G2, LOW);
   
   if(bstate == 0){
@@ -245,7 +246,7 @@ void delay_a(int sec){        //검사 딜레이
     delay(k);
     if(bstate == 0){        //스위치 입력이 안 들어와 있는 경우 스위치 입력 들어왔는지 검사
       bool a = bCheck();    //입력이 있다면 true 값 반환 후 -> a
-     if(a == true){       
+     if(a == true){      
         return;
      }
    }
@@ -263,7 +264,7 @@ void delay_b(int sec){        //검사안하는 딜레이
       printNumMatrix(numberd);  //남은 시간 신호
       
       if(timec <1000) timec += k;   //지나간 시간 계산해 주는 부분
-      else if(timec > 999+times){   //10000 보다 클 경우 잖아
+      else if(timec > 999){   //10000 보다 클 경우 잖아
        timec = 0;                   //다 돌았다고 생각하는 거니깐 time을 리셋 해주고  
        if(numberd == 0) numberd = 0;
        else numberd -= 1;
@@ -271,7 +272,6 @@ void delay_b(int sec){        //검사안하는 딜레이
       }
     }
     delay(k);
-   times=0;
   }
 }
 
@@ -331,37 +331,3 @@ void printNumMatrix(int num) { //남은 시간 출력
     cMat2();
   }
 }
-
-/*void delay_b(int sec){
-  int k = 30;
-    if(sstate == 1){            
-        for(int i = sec / k ; i> (sec / k)/3 ; i--){
-             printSignalMatrix(0);     //정지 신호등 (좌우) 
-             cMat2();                  //숫자 신호등 끄기
-             if(timec <1000) timec += k;   //지나간 시간 계산해 주는 부분
-             else if(timec > 999){         //10000 보다 클 경우 잖아
-             timec = 0;                   //다 돌았다고 생각하는 거니깐 time을 리셋 해주고  
-             if(numberd == 0) numberd = 0;//이게 머지
-             else numberd -= 1;
-               delay(k);
-               if(bstate == 0){        //스위치 입력이 안 들어와 있는 경우 스위치 입력 들어왔는지 검사
-                  bool a = bCheck();    //입력이 있다면 true 값 반환 후 -> a
-                  if(a == true)  return;    
-               }
-        }
-    }
-    else if(sstate == 2){
-      for(int i = sec / k ; i>0 ; i--){
-             printSignalMatrix(0);     //정지 신호등 (좌우) 
-             cMat2();                  //숫자 신호등 끄기
-             delay(k);
-             if(bstate == 0){        //스위치 입력이 안 들어와 있는 경우 스위치 입력 들어왔는지 검사
-                bool a = bCheck();    //입력이 있다면 true 값 반환 후 -> a
-                if(a == true){         
-                    return;
-                }   
-             }
-        }
-    }
-}*/
-  
