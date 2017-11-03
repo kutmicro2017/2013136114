f80e892ef48fa3eae9c4729e597a5416273e57dc
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-11-03 12:16:58 +0900

    Update TEST.ino

--- TEST.ino
+++ TEST.ino
@@ -5,22 +5,19 @@
 #define Y2 16
 #define G2 17 //좌우 신호등
 
-#define TimeY 5 // sec단위로 적고
-#define TimeG 10000 //sec*1000 해서 적기
+#define TimeY 3 // sec단위로 적고
+#define TimeG 6000 //sec*1000 해서 적기
 #define TimeS 10
 
 #define buttonA 2
 
 const int col1[8] = { 28,29,26,27,24,25,22,23 };
-const int row1[8] = { 38,39,36,37,34,35,32,33 };  //사람
+const int row1[8] = { 38,39,36,37,34,35,32,33 };
 const int col2[8] = { 46,47,44,45,42,43,40,41};
-const int row2[8] = { 60,61,62,63,64,65,66,67};   //숫자
+const int row2[8] = { 60,61,62,63,64,65,66,67};
 
-int numberd = 0;// 신호등 시간
-int timec = 0;  // 리셋 시간 관리
-int bstate = 0; // <보행자신호> 0 = 아무것도 안눌림 1 = 1번 라인일때 2 = 2번 라인일때
-int sstate = 0; //  <상하 // 좌우> 신호 구분
-
+int numberd = 0;
+int timec = 0;
 
 int signImage[2][8][8] = {
   { //stop
@@ -159,6 +156,9 @@ int walkSignNumber[10][8][4] = {
   }
 };
 //////////////////////////////////////////////////////////////////
+int bstate = 0; // <보행자신호> 0 = 아무것도 안눌림 1 = 1번 라인일때 2 = 2번 라인일때
+int sstate = 0; //  <상하 // 좌우> 신호 구분
+
 
 void delay_a(int sec);
 bool bCheck();
@@ -180,107 +180,86 @@ void setup() {
 }
 
 void loop() {
-  sstate = 1;                     // 1번라인
+  sstate = 1; // 1번라인
   digitalWrite(R2, HIGH);
   digitalWrite(R1, LOW);
   digitalWrite(G1, HIGH);
-  delay_a(TimeG);         
+  delay_a(TimeG);
   digitalWrite(G1, LOW);
-  if(bstate == 0){                 //보행자 신호를 입력 받지않았을 경우
-     for(int i = 0; i<TimeY; i++){
-     digitalWrite(Y1, HIGH);
-     delay_b(500);
-     digitalWrite(Y1, LOW);
-     delay_b(500);
+  if(bstate == 0){      //보행자 신호를 입력 받지않았을 경우
+    for(int i = 0; i<TimeY; i++){
+      digitalWrite(Y1, HIGH);
+     delay_a(375);
+     digitalWrite(Y1, LOW);\
+     delay_a(375);
    }
-  }else if(bstate == 1){            //보행자 신호를 입력 받았을 경우 스위치가 눌렸을 경우 bstate=1;
+  }else if(bstate == 1){    //보행자 신호를 입력 받았을 경우
     for(int i = 0; i<3; i++){
-     digitalWrite(Y1, HIGH);            //이 부분이 필요가 없는거 같은데
-     delay_a(500);
+     digitalWrite(Y1, HIGH);
+     delay_a(375);
      digitalWrite(Y1, LOW);
-     delay_a(500);
+     delay_a(375);
+     //고
    }
    bstate = 0;
   }
   digitalWrite(R1, HIGH);
- ////////////////////////////////////////////// //전환
-  sstate = 2; // 2번라인             
-  numberd = 10;                     //이게 숫자 신호등의 시간인거 같아
+  //전환
+  sstate = 2; // 2번라인
+  numberd = 8;
   digitalWrite(R2, LOW);
   digitalWrite(G2, HIGH);
   delay_a(TimeG);
-  if(bstate == 2){                  //버튼이 눌러진 상태일경우
-    numberd = 10;                   //돌아가는 딜레이네
-    delay_a(TimeG);                 //이게 리셋이구나
+  if(bstate == 2){
+    numberd = 8;
+    delay_a(TimeG);
+    bstate = 0;
   }
-   cMat2();
   digitalWrite(G2, LOW);
-  
+
+  sstate = 1;
   if(bstate == 0){
    for(int i = 0; i<TimeY; i++){     //즉 i 하나에 1초
-      digitalWrite(Y2, HIGH);
-      delay_b(500);
+     digitalWrite(Y2, HIGH);
+      delay_a(375);
       digitalWrite(Y2, LOW);
-      delay_b(500);
+     delay_a(375);
     }
    }
 }
-void delay_a(int sec){        //검사 딜레이
-  int k = 30;
+void delay_a(int sec){
+  int k = 15;
   for(int i = sec / k ; i>0 ; i--){
-    if(sstate == 1){            //상하 신호가 초록불일 때
-      printSignalMatrix(0);     //정지 신호등 (좌우) 
-      cMat2();                  //숫자 신호등 끄기
+    if(sstate == 1){
+      printSignalMatrix(0);
+      cMat2();
     }
-    else if(sstate == 2){       //좌우 신호가 초록불일 때
-      printSignalMatrix(1);     //가는 신호등
-      printNumMatrix(numberd);  //남은 시간 신호
+    else if(sstate == 2){
+      printSignalMatrix(1);
+      printNumMatrix(numberd);
       
-      if(timec <1000) timec += k;   //지나간 시간 계산해 주는 부분
-      else if(timec > 999){         //10000 보다 클 경우 잖아
-       timec = 0;                   //다 돌았다고 생각하는 거니깐 time을 리셋 해주고  
-       if(numberd == 0) numberd = 0;//이게 머지
+      if(timec <750) timec += k;
+      else if(timec > 749){
+       timec = 0;
+       if(numberd == 0) numberd = 0;
        else numberd -= 1;
       }
     }
     delay(k);
-    if(bstate == 0){        //스위치 입력이 안 들어와 있는 경우 스위치 입력 들어왔는지 검사
+    if(bstate == 0){    //스위치 입력이 안 들어와 있는 경우 스위치 입력 들어왔는지 검사
       bool a = bCheck();    //입력이 있다면 true 값 반환 후 -> a
-     if(a == true){      
+     if(a == true){         
         return;
      }
    }
   }
 }
-void delay_b(int sec){        //검사안하는 딜레이
-  int k = 30;
-  for(int i = sec / k ; i>0 ; i--){
-    if(sstate == 1){            //상하 신호가 초록불일 때
-      printSignalMatrix(0);     //정지 신호등 (좌우) 
-      cMat2();                  //숫자 신호등 끄기
-    }
-    else if(sstate == 2){       //좌우 신호가 초록불일 때
-      printSignalMatrix(1);     //가는 신호등
-      printNumMatrix(numberd);  //남은 시간 신호
-      
-      if(timec <1000) timec += k;   //지나간 시간 계산해 주는 부분
-      else if(timec > 999){   //10000 보다 클 경우 잖아
-       timec = 0;                   //다 돌았다고 생각하는 거니깐 time을 리셋 해주고  
-       if(numberd == 0) numberd = 0;
-       else numberd -= 1;
-       bstate=0;
-      }
-    }
-    delay(k);
-  }
-}
-
 
 bool bCheck(){
   int a = digitalRead(buttonA);
 
   if(a == LOW){         //스위치 입력 받았을 경우
-    if(sstate == 1){    //1번 라인 실행중일 경우   근데 사실 상 1번라인 실행 중에 스위치가?
+    if(sstate == 1){    //1번 라인 실행중일 경우
       bstate = 1;
       delay(TimeS);
      return true;
@@ -307,10 +286,10 @@ void cMat2(){ //2번
 }
 
 
-void printSignalMatrix(int num) {           //정지(0)인지 통행 가능(1)인지 아이콘 출력
+void printSignalMatrix(int num) { //정지(0)인지 통행 가능(1)인지 아이콘 출력
   for (int r = 0; r < 8; r++) {
-    cMat();                                 //다꺼주는거
-    digitalWrite(row1[r], HIGH);            //row를 high로 바꾼 뒤
+    cMat();                         //다꺼주는거
+    digitalWrite(row1[r], HIGH);    //row를 high로 바꾼 뒤
     for (int c = 0; c < 8; ++c) {
       if (signImage[num][r][c]) digitalWrite(col1[c], LOW);  //키고 싶은 부분만 col을 low로 바꾸네
     }
